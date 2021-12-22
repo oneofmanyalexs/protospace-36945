97c0fcae45d987714f1374060c38ec1f176af90f
oneofmanyalexs <ahlbattery@gmail.com>
2021-12-22 20:54:09 +0900

    フロント実装の準備

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'comments/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "comments#index"
 end
