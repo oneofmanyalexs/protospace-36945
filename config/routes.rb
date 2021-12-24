4d55e70444ee5e84e1e03cf887f2d989bf5fc779
oneofmanyalexs <ahlbattery@gmail.com>
2021-12-24 21:26:27 +0900

    Prototypes実装の準備

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'comments/index'
+  get 'prototypes/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "comments#index"
+  root to: "prototypes#index"
 end
