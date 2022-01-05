f253a5e9c391b5d80df1bad54a91369ede9c17ba
oneofmanyalexs <ahlbattery@gmail.com>
2022-01-05 18:13:02 +0900

    made twitter table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_02_135342) do
+ActiveRecord::Schema.define(version: 2022_01_05_090802) do
+
+  create_table "twitters", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.string "text"
+    t.text "image"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
