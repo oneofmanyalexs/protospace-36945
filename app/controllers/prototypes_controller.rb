c8869a129850ecf0e73e732d851c1f930b504584
oneofmanyalexs <ahlbattery@gmail.com>
2022-01-05 15:03:28 +0900

    solved adding new to prototypes added new_prototypes_path

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,14 +1,23 @@
 class PrototypesController < ApplicationController
-
+  before_action :set_prototype, except: [:index, :new, :create]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :contributor_confirmation, only: [:edit, :update, :destroy]
   def index
     @users = User.all
   end
 
 
   def new 
+    @prototype = Prototype.new
   end
-  
+
   def create
+    @prototype = Prototype.new(prototype_params)
+    if @prototype.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
 
