36b94509117692242ae9ffa3c234bb49ed09e71e
oneofmanyalexs <ahlbattery@gmail.com>
2022-01-05 19:20:13 +0900

    created room table

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,5 +1,13 @@
 class Prototype < ApplicationRecord
-  validates :text, presence: true
+  belongs_to :room
   belongs_to :user
   has_one_attached :image
+
+  validates :text, presence: true
+  validates :content, presence: true, unless: :was_attached?
+
+  def was_attached?
+    self.image.attached?
+  end
+
 end
