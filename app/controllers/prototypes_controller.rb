2e637608f2a2f2e5110d002cde9890194a3ee011
oneofmanyalexs <ahlbattery@gmail.com>
2022-01-22 21:43:01 +0900

    Authenticate実装済み

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -2,6 +2,7 @@ class PrototypesController < ApplicationController
   #before_action :set_prototype, except: [:index, :new, :create]
 
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :move_to_index, except: [:index, :show]
 
   def index
     @users = User.all
